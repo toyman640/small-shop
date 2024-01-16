5c73bfa6ce17198d43f7d2f137ec092ea99321d5
toyman640 <falakoadebayor@gmail.com>
2024-01-16 14:50:51 +0100

    Complete api build

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :categories
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+      resources :categories
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
